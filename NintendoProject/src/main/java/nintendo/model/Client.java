c4958b9dd3a3576acfe76c38d3b8ef457edbe432
romain44240 <romain.veneu56@gmail.com>
2025-03-21 10:26:01 +0100

    liste d'achats dans client, boutique => jeu

--- NintendoProject/src/main/java/nintendo/model/Client.java
+++ NintendoProject/src/main/java/nintendo/model/Client.java
@@ -1,32 +1,55 @@
 package nintendo.model;
 
+import java.util.List;
+
 public class Client {
 	protected String nom;
 	protected String prenom;
+	protected List<Achat> listeAchats;
+	
 	
 	public Client(String nom, String prenom) {
 		this.nom = nom;
 		this.prenom = prenom;
 	}
 
+
 	public String getNom() {
 		return nom;
 	}
 
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
 
 	public String getPrenom() {
 		return prenom;
 	}
 
+
+	public List<Achat> getListeAchats() {
+		return listeAchats;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
 	public void setPrenom(String prenom) {
 		this.prenom = prenom;
 	}
 
+
+	public void setListeAchats(List<Achat> listeAchats) {
+		this.listeAchats = listeAchats;
+	}
+
+
 	@Override
 	public String toString() {
-		return "Client [nom=" + nom + ", prenom=" + prenom + "]";
+		return "Client [nom=" + nom + ", prenom=" + prenom + ", listeAchats=" + listeAchats + "]";
 	}
+
+	
+	
+	
 }
